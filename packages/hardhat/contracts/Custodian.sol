76b057e122d98addeca2017009b9823a0304fad2
Miguel Delgado <mdelgado.cab@gmail.com>
2024-05-01 19:17:38 -0600

    feat: added custodian contract

--- packages/hardhat/contracts/Custodian.sol
+++ packages/hardhat/contracts/Custodian.sol
@@ -5,12 +5,60 @@ import "./Token.sol";
 
 contract Custodian {
 	Token public token;
+	address public signers;
+	mapping(address => bool) public isSigner;
+	mapping(address => bool) public hasApprovedWithdrawal;
 
-	constructor(Token _token) {
-		token = _token;
+	uint256 public requiredSignatures;
+	uint256 public approvedSignatures;
+
+	modifier onlySigner() {
+		require(isSigner[msg.sender], "Not a signer");
+		_;
 	}
 
-	function sendTokens(address recipient, uint256 amount) external {
+	constructor(
+		Token _token,
+		address[] memory _signers,
+		uint256 _requiredSignatures
+	) {
+		token = _token;
+		requiredSignatures = _requiredSignatures;
+
+		require(_signers.length > 0, "No signers");
+
+		for (uint256 i = 0; i < _signers.length; i++) {
+			require(_signers[i] != address(0), "Invalid signer");
+
+			isSigner[_signers[i]] = true;
+			hasApprovedWithdrawal[_signers[i]] = false;
+			siigners.push(_signers[i]);
+		}
+	}
+
+	function deposit(uint256 amount) external {
+		require(
+			token.transferFrom(msg.sender, address(this), amount),
+			"Transfer failed"
+		);
+	}
+
+	function approveWithdrawal(address recipient) external onlySigner {
+		require(!hasApprovedWithdrawal[recipient], "Already approved");
+		hasApprovedWithdrawal[recipient] = true;
+		approvedSignatures++;
+	}
+
+	function sendTokens(address recipient, uint256 amount) external onlySigner {
+		require(
+			approvedSignatures >= requiredSignatures,
+			"Insufficient signatures"
+		);
 		require(token.transfer(recipient, amount), "Transfer failed");
+
+		approvedSignatures = 0;
+		for (uint256 i = 0; i < signers.length; i++) {
+			hasApprovedWithdrawal[signers[i]] = false;
+		}
 	}
 }
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -3,9 +3,8 @@
 import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
@@ -14,43 +13,6 @@ type HeaderMenuLink = {
   icon?: React.ReactNode;
 };
 
-export const menuLinks: HeaderMenuLink[] = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
-];
-
-export const HeaderMenuLinks = () => {
-  const pathname = usePathname();
-
-  return (
-    <>
-      {menuLinks.map(({ label, href, icon }) => {
-        const isActive = pathname === href;
-        return (
-          <li key={href}>
-            <Link
-              href={href}
-              passHref
-              className={`${
-                isActive ? "bg-secondary shadow-md" : ""
-              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
-            >
-              {icon}
-              <span>{label}</span>
-            </Link>
-          </li>
-        );
-      })}
-    </>
-  );
-};
 
 /**
  * Site header
@@ -84,7 +46,6 @@ export const Header = () => {
                 setIsDrawerOpen(false);
               }}
             >
-              <HeaderMenuLinks />
             </ul>
           )}
         </div>
@@ -93,17 +54,12 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Staking Vault</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
-          <HeaderMenuLinks />
-        </ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
-        <FaucetButton />
       </div>
     </div>
   );
